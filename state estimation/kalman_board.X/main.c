dbf745533528b933037439c0dcadef97fe3fbb76
Joe-Joe-Joe-Joe <joedolina@gmail.com>
2023-01-07 20:55:02 -0500

    Create Kalman board project; move python files to subdirectory

---
+++ state estimation/kalman_board.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: joedo
+ *
+ * Created on January 7, 2023, 8:25 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ state estimation/kalman_board.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=kalman_board.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/kalman_board.X.production.hex
