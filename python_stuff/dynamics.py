973e1c9e14d7cb61df1a6b3a6faf2a0193d3fc0c
Stephen Wang <ste53539@gmail.com>
2023-05-07 19:52:46 -0400

    implemented 3d rigid body dynamics model

---
+++ python_stuff/dynamics.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+def skew_symmetric(vec):
+  return np.array([
+    [0, -vec[2], vec[1]],
+    [vec[2], 0, -vec[0]],
+    [-vec[1], vec[0], 0]
+  ])
+
+class RigidBody:
+  def __init__(self):
+
+    # params
+    self.J = skew_symmetric(np.array([1, 1, 1])) # spherical cow energy
+
+    # states
+    self.Delta = np.zeros(3)
+
+    self.v = np.zeros(3)
+
+    # self.alpha = 0
+    # self.beta = 0
+    # self.gamma = 0
+    # self.Rx = np.zeros((3,3))
+    # self.Ry = np.zeros((3,3))
+    # self.Rz = np.zeros((3,3))
+    self.R = np.zeros((3,3))
+
+    self.omega = np.zeros(3)
+
+    # state derivatives
+    self.Delta_dot = np.zeros(3)
+    self.v_dot = np.zeros(3)
+    self.R_dot = np.zeros((3,3))
+    self.omega_dot = np.zeros(3)
+
+    # inputs
+    self.tau = np.zeros(3)
+    self.a = np.zeros(3)
+
+  def update(self, dt=0.01):
+    # calculate the derivatives
+    self.omega_dot = np.linalg.inv(self.J) @ (self.tau - np.cross(self.omega, self.J @ self.omega))
+
+    self.v_dot = skew_symmetric(self.omega) @ self.v + self.a
+
+    self.R_dot = self.R @ skew_symmetric(self.omega)
+
+    self.Delta_dot = self.R @ self.v
+
+    self.omega += self.omega_dot * dt
+    self.Delta += self.Delta_dot * dt
+    self.v += self.v_dot * dt
+    self.R += self.R_dot * dt
+
+class Payload(RigidBody):
+  def __init__(self):
+    super().__init__()
+
+  def get_noisy_data(self):
+    return (
+      self.Delta + np.normal(0, 1, 3),
+      self.v + np.normal(0, 1, 3),
+      self.R + np.normal(0, 1, 3),
+      self.omega + np.normal(0, 1, 3),
+    )
+
+  def control(self, a, tau, dt=0.01):
+    self.a = a
+    self.tau = tau
