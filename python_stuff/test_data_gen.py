5ffbd54db022ef499c190d118c93ce8e0f87b8a1
Ozayr Raazi <oraazi2004@gmail.com>
2023-05-17 20:36:57 -0400

    Updated the sensor readings

--- python_stuff/test_data_gen.py
+++ python_stuff/test_data_gen.py
@@ -3,8 +3,6 @@ import numpy as np
 # angular stuff
 theta = 0
 omega = 2
-a_pos = 0
-a_vel = 0
 
 ## Position stuff
 x = [0, 0, 0]
@@ -51,11 +49,18 @@ def conv(vec, vel, angle):
 
     result = np.matmul(v_square_scaled_plus_v_plus_i, Rot)
 
-    return result # *vec
+    return np.matmul(result, vec)
 
 
 for i in range(1000):
     t = 0.01 * (1 + i)
-    sensor_reads = f'{x[0] + t*v[0] + t*t*a[0]/2 + n()},{x[1] + t*v[1] + t*t*a[1]/2 + n()},{x[2] + t*v[2] + t*t*a[2]/2 + n()},{a[0] + n()},{a[1] + n()},{a[2] + n()}'
+
+    X = [x[0] + t*v[0] + t*t*a[0]/2, x[1] + t*v[1] + t*t*a[1]/2, x[2] + t*v[2] + t*t*a[2]/2]
+    # theta = theta + n()
+    # omega = omega + n()
+    x_rot = conv(X, v, theta) + n()
+    a_rot = conv(a, v, theta) + n()
+
+    sensor_reads = f'{x_rot[0]},{x_rot[1]},{x_rot[2]},{a_rot[0]},{a_rot[1]},{a_rot[2]}'
     true_values = f'{x[0] + t*v[0] + t*t*a[0]/2},{x[1] + t*v[1] + t*t*a[1]/2},{x[2] + t*v[2] + t*t*a[2]/2},{v[0] + t*a[0]},{v[1] + t*a[1]},{v[2] + t*a[2]},{a[0]},{a[1]},{a[2]}'
     print(f'{t},{sensor_reads},{true_values}')
