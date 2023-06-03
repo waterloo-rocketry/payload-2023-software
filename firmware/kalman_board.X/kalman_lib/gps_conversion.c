82d770f87ae28332cf65c2ae24c82ff17dc0d4d8
Ozayr Raazi <oraazi2004@gmail.com>
2023-06-03 13:43:04 -0400

    Updated gps conversion to return x and y displacement

--- firmware/kalman_board.X/kalman_lib/gps_conversion.c
+++ firmware/kalman_board.X/kalman_lib/gps_conversion.c
@@ -1,30 +1,39 @@
 #include <math.h>
+#include "kalman_lib.h"
 #include <stdio.h>
 
-double gps_conversion(double lat1, double long1, double lat2, double long2) {
-    // Radius of the earth in km
-    double radius = 6378.13;
-
-    // These calculations come from the Haversine formula
-    // https://en.wikipedia.org/wiki/Haversine_formula
-    double dlat = ((lat2 * M_PI) / 180) - ((lat1 * M_PI) / 180);
-    double dlong = ((long2 * M_PI) / 180) - ((long1 * M_PI) / 180);
-    double a = sin(dlat/2) * sin(dlat/2) + cos(lat1*M_PI/180) * 
-               cos(lat2*M_PI/180) * sin(dlong/2) * sin(dlong/2);
-    double b = 2 * atan2(sqrt(a), sqrt(1-a));
-    double c = radius * b;
-
-    // Return the answer in meters
-    return c * 1000;
+double to_radians(double angle) {
+    return angle * M_PI / 180.0;
 }
 
+struct Vector gps_conversion(double lat1, double long1, double lat2, double long2) {
+    // Delta x and y
+    double delta_y = lat2 - lat1;
+    double delta_x = long2 - long1;
+
+    // 1 degree latitude is always 111.32 km
+    double y_disp = delta_y * 111.32;
+
+    // 1 degree of longitude depends on current latitude
+    // Earth's circumference is 40,075 km
+    double km_per_long = 40075 * cos(to_radians(lat2)) / 360;
+    double x_disp = delta_x * km_per_long;
+
+    // Return x and y displacement as a vector
+    double displacement[2] = {x_disp*1000, y_disp*1000};
+    return (struct Vector) {displacement, 2};
+}
+
+// Test
 int main() {
     double lat1 = 50.0;
     double long1 = -170.3;
     double lat2 = 50.01;
     double long2 = -170.29;
 
-    printf("%lf", gps_conversion(lat1, long1, lat2, long2)); // should be around 1 323 m
+    struct Vector result = gps_conversion(lat1, long1, lat2, long2);
+
+    printf("x = %lf, y = %lf\n", result.data[0], result.data[1]); 
 
     return 0;
 }
